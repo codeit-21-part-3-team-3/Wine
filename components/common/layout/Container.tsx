feba6d1f2ae3b6c28d9f27a5709c1969d9251203
unknown <jylee2670@gmail.com>
2026-02-05 16:30:23 +0900

    feat: 공용 컴포넌트 Container 작업

--- components/common/layout/Container.tsx
+++ components/common/layout/Container.tsx
@@ -4,15 +4,12 @@ import type { ReactNode } from 'react';
 interface ContainerProps {
   children: ReactNode;
   className?: string;
-  as?: React.ElementType; // 선택: div | section | main 등
 }
 
-export default function Container({ children, className, as: Component = 'div' }: ContainerProps) {
+export default function Container({ children, className }: ContainerProps) {
   return (
-    <Component
-      className={cn('w-full mx-auto max-w-300 box-border', 'px-4 lg:px-0 sm:px-6', className)}
-    >
+    <div className={cn('w-full mx-auto max-w-300 box-border', 'px-4 lg:px-0 sm:px-6', className)}>
       {children}
-    </Component>
+    </div>
   );
 }
