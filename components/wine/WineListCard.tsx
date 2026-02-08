194948092d738f163a7e89f36a7a8536ddcab978
unknown <jylee2670@gmail.com>
2026-02-08 17:41:21 +0900

    refactor: css 수정

--- components/wine/WineListCard.tsx
+++ components/wine/WineListCard.tsx
@@ -12,7 +12,7 @@ export default function WineListCard({ wine }: Props) {
     <article className="w-full mt-10 mb-8 md:mb-15 lg:mb-20 rounded-2xl border border-gray-300 bg-white hover:shadow-md transition">
       <div className="grid grid-cols-[80px_1fr] md:grid-cols-[60px_minmax(0,1fr)_auto] gap-x-4 md:gap-x-20 px-5 md:pl-15 md:pr-13 pt-2.5 items-start">
         <div className="relative overflow-hidden row-span-2 lg:w-15 h-57 w-18 shrink-0">
-          <Image src={wine.image} alt={wine.name} fill className="object-fit mt-5" />
+          <Image src={wine.image} alt={wine.name} fill className="object-cover mt-5" />
         </div>
         <div className="col-start-2 row-start-1 mt-5 md:mt-4">
           <WineInfo name={wine.name} region={wine.region} price={wine.price} />
