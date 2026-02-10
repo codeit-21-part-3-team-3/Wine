741f789dfe89680407b16fac2f409c1abdf76624
kimaerang <dofks213@naver.com>
2026-02-10 12:41:56 +0900

    refactor: 불필요 주석삭제

--- hooks/useWineTaste.ts
+++ hooks/useWineTaste.ts
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { TasteType } from '@/components/common/ui/TasteItem';
 
 export const useWineTaste = (initialData?: Partial<Record<TasteType, number>>) => {
-  // ✅ 피드백 반영: undefined를 허용하지 않고 기본값 0을 할당
   const [tasteForm, setTasteForm] = useState<Record<TasteType, number>>({
     바디감: initialData?.바디감 ?? 0,
     타닌: initialData?.타닌 ?? 0,
