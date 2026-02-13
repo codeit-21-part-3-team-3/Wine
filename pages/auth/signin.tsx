076b40b1fb72854481c9059d56a55655726e16ad
cobool <saokm5@gmail.com>
2026-02-13 15:23:36 +0900

    refactor: 테스트코드 제거 ui 단위로 PR 변경

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,55 +1,20 @@
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-
 import AuthLayout from '@/components/common/layout/AuthLayout';
 import FormField from '@/components/common/form/FormField';
 import Button from '@/components/common/ui/Button';
 import Link from 'next/link';
 
-import { useAuth } from '@/providers/Auth/AuthProvider';
-
 export default function SignIn() {
-  const router = useRouter();
-  const { login } = useAuth();
-
-  const [values, setValues] = useState({
-    email: '',
-    password: '',
-  });
-
-  const handleChange = id => e => {
-    setValues(prev => ({
-      ...prev,
-      [id]: e,
-    }));
-  };
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-    const res = await login(values);
-    if (res?.success) {
-      router.push('/');
-    }
-  };
-
   return (
     <AuthLayout>
-      <form onSubmit={handleSubmit} className="flex flex-col">
+      <form className="flex flex-col">
         <div className="flex flex-col gap-6 mb-10">
-          <FormField
-            id="email"
-            label="이메일"
-            placeholder="이메일을 입력해주세요"
-            type="text"
-            onChange={handleChange('email')}
-          />
+          <FormField id="email" label="이메일" placeholder="이메일을 입력해주세요" type="text" />
 
           <FormField
             id="password"
             label="비밀번호"
             placeholder="비밀번호를 입력해주세요"
             type="password"
-            onChange={handleChange('password')}
           />
         </div>
 
