37985fe12e05adc82bb34bf262d5a8485deefebe
kimaerang <dofks213@naver.com>
2026-02-23 04:43:24 +0900

    fix: 리뷰모달 수정

--- components/wine/detail/ReviewSection.tsx
+++ components/wine/detail/ReviewSection.tsx
@@ -4,7 +4,6 @@ import ReviewStats from '@/components/wine/detail/ReviewStats';
 import EmptyState from '@/components/common/ui/EmptyState';
 import ReviewFormModal from '@/components/review/ReviewFormModal';
 import { GetWineDetailResponse } from '@/lib/api/wine/wine.types';
-import { CreateReviewRequest, UpdateReviewRequest } from '@/lib/api/review/review.types';
 import { useReviewHandlers } from '@/hooks/review/useReviewHandlers';
 import { DeleteReviewDialog } from '@/components/review/DeleteReviewDialog';
 
@@ -61,7 +60,7 @@ export default function ReviewSection({ wine, myId }: ReviewSectionProps) {
           onOpenChange={setIsWriteModalOpen}
           mode="create"
           wine={wine}
-          onSubmit={handleUpdateReview}
+          onSubmit={handleCreateReview}
         />
       </div>
     );
